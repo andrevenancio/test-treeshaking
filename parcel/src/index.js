859c14fad4b828c7b7074658158fc7df9d07ef1e
André Venâncio <noreply@andrevenancio.com>
2019-04-04 12:11:30 +0100

    importing from src directly allows for tree shaking

--- parcel/src/index.js
+++ parcel/src/index.js
@@ -1,4 +1,5 @@
-// import { Vector3 } from 'three';
-import { Vector3 } from './three';
+// import { Vector3 } from 'three'; // inherits the whole of threejs library
+// import { Vector3 } from './three.module'; // inherits the whole of threejs library
+import { Vector3 } from 'three/src/math/vector3'; // works but its annoying having to memorise the location of each file
 
 console.log('Parcel', new Vector3(1, 2, 3));
--- rollup/src/index.js
+++ rollup/src/index.js
@@ -1,4 +1,5 @@
-// import { Vector3 } from 'three';
-import { Vector3 } from './three';
+// import { Vector3 } from 'three'; // inherits the whole of threejs library
+// import { Vector3 } from './three.module'; // inherits the whole of threejs library
+import { Vector3 } from 'three/src/math/vector3'; // works but its annoying having to memorise the location of each file
 
 console.log('Rollup', new Vector3(1, 2, 3));
